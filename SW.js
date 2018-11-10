4cbddeaf3953739cf50b21b4d52ca1194f6956ee
Gaetano Sabino <gaetano.sabino@gmail.com>
2018-11-10 00:32:47 -0500

    Sin / por git

--- SW.js
+++ SW.js
@@ -7,15 +7,15 @@ const inmutable = 'inmutable-v1';
 const app_shell = [
     // '/',
     'index.html',
-    // '/css/style.css',
-    // 'img/favicon.ico',
-    // 'img/avatars/hulk.jpg',
-    // 'img/avatars/ironman.jpg',
-    // 'img/avatars/spiderman.jpg',
-    // 'img/avatars/thor.jpg',
-    // 'img/avatars/wolverine.jpg',
-    // '/js/app.js',
-    // '/js/sw-util.js'
+    'css/style.css',
+    'img/favicon.ico',
+    'img/avatars/hulk.jpg',
+    'img/avatars/ironman.jpg',
+    'img/avatars/spiderman.jpg',
+    'img/avatars/thor.jpg',
+    'img/avatars/wolverine.jpg',
+    'js/app.js',
+    'js/sw-util.js'
 
 ];
 
